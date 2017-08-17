f35df36c6f6c7824f4165ced6e8fca8641d7115e
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2017-08-16 20:30:10 -0700

    Adding a small 3d example

---
+++ client/core/src/main/scala/me/ngrid/examples/Basic3D.scala
@@ -0,0 +1,17 @@
+package me.ngrid.examples
+
+import com.badlogic.gdx.ApplicationListener
+
+class Basic3D extends ApplicationListener{
+  override def resume(): Unit = {}
+
+  override def pause(): Unit = {}
+
+  override def create(): Unit = {}
+
+  override def resize(width: Int, height: Int): Unit = {}
+
+  override def dispose(): Unit = {}
+
+  override def render(): Unit = {}
+}
