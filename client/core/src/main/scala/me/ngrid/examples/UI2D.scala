4b6eda2c1bd4a8e6abfe6c063bf63a52949f398f
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2017-10-06 00:06:58 -0700

    messing around with 2d ui, next up, initial login screen

--- client/core/src/main/scala/me/ngrid/examples/UI2D.scala
+++ client/core/src/main/scala/me/ngrid/examples/UI2D.scala
@@ -1,25 +1,46 @@
 package me.ngrid.examples
 
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.scenes.scene2d.{InputEvent, Stage, ui}
 import com.badlogic.gdx.{ApplicationAdapter, Gdx}
-import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.Table
 
 class UI2D extends ApplicationAdapter {
   var stage: Stage  = _
-  var table: Table =_
+  var table: ui.Table =_
 
   override def create() = {
     stage = new Stage()
     Gdx.input.setInputProcessor(stage)
 
-    table = new Table()
+    val skin = new ui.Skin(Gdx.files.internal("sgx/skin/sgx-ui.json") )
+    table = new ui.Table(skin)
     table.setFillParent(true)
     stage.addActor(table)
 
-    table.setDebug(true)
+//    table.setDebug(true)
+
+    val button = new ui.Button(skin)
+    val list = new ui.List[String](skin)
+    button.setChecked(true)
+    button.add("What the fuck buttons").bottom()
+    button.addListener(new ClickListener(){
+      override def clicked(event: InputEvent, x: Float, y: Float) = {
+        println(s"$event, ($x, $y), ${list.getSelected}")
+      }
+    })
+
 
     // Add the widgets
+    table.add("This is some sweet ass text")
+    table.top.row.bottom
+    table.add(button)
+
+    table.row
+//    list.setFillParent(true)
+    list.getItems.addAll("Wooh", "Mooh", "Blarga blarga")
+
+    table.add(list)
   }
 
   override def resize(width: Int, height: Int) = {
